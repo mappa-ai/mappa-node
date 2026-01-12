904f5ad95170e6503d578f8929efae30952f7ffd
drsh4dow <daniel.morettiv@gmail.com>
2026-01-12 12:34:59 -0300

    added tsdown config

---
+++ tsdown.config.ts
@@ -0,0 +1,9 @@
+import type { UserConfig } from "tsdown";
+
+export default {
+  entry: "src/index.ts",
+  dts: true,
+  platform: "node",
+  format: ["esm", "cjs"],
+  minify: true,
+} satisfies UserConfig;
